1fbd1ffa202e799e8389a036d440b53f55a90310
DevExpressExampleBot <fakeEmail>
2023-12-12 18:49:41 +0400

    Source auto update [skip ci]

--- VB/DXMapExample/MainWindow.xaml.vb
+++ VB/DXMapExample/MainWindow.xaml.vb
@@ -1,15 +1,15 @@
-﻿Imports System.Windows
+Imports System.Windows
 
 Namespace DXMapExample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
